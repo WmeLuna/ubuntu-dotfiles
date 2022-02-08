1cc2afd8e965fdc8d45935fc4c003ddbf024070a
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-02-07 19:19:11 -0500

    Update GooseTetherless.plugin.js

--- GooseTetherless.plugin.js
+++ GooseTetherless.plugin.js
@@ -1,10 +1,10 @@
  /**
  * @name GooseModThetherlessInjector
- * @version 0.0.2
+ * @version 1.0.1
  * @description injects GooseMod Tetherlessly, allowing for betterdiscord updates to function properly and providing stabily as for some reason it uninjects for me alot
  * @author WmeLuna
  * @authorId 865632950443835392
- * @updateUrl https://github.com/WmeLuna/ubuntu-dotfiles/raw/main/GooseTetherless.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/WmeLuna/ubuntu-dotfiles/main/GooseTetherless.plugin.js
  */
 
 module.exports = class GooseModThetherlessInjector{
